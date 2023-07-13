c3ba0c1b68a19bce3c3f2b03286cc9eff3c010ce
PaschalALX <pasmac2018@gmail.com>
2023-07-14 00:00:53 +0100

    Added Task 5 1

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,14 +1,17 @@
--- validate email
+-- Reset Email Validity when changed
+
+DROP TRIGGER IF EXISTS reset_email_validity
 
 DELIMITER &&
 
-CREATE TRIGGER validate_email
+CREATE TRIGGER reset_email_validity
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
 	IF NEW.email != OLD.email
 	THEN SET NEW.valid_email = 0;
 	END IF
-END &&
+END; 
+&&
 
 DELIMITER ;
