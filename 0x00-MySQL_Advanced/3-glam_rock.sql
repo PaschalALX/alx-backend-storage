d7bd541025867f21a875c40d52f5835e912a3874
PaschalALX <pasmac2018@gmail.com>
2023-07-13 22:17:07 +0100

    Task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- all bands with Glam rock as their main style,
+-- ranked by their longevity
+
+select band_name, IF(split,split-formed, 2022-formed)
+AS lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" 
+ORDER BY lifespan DESC;
