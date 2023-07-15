7566aa3289e3ab7c620ce9346014062f08bfa143
PaschalALX <pasmac2018@gmail.com>
2023-07-15 18:03:53 +0100

    Ref 3

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,27 +1,43 @@
--- add bonus
+-- SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
 
-DROP PROCEDURE IF EXISTS AddBonus;
+DROP PROCEDURE IF EXISTS `AddBonus`;
 
-DELIMITER &&
+DELIMITER |
 
-CREATE PROCEDURE AddBonus(
-	IN user_id INT, 
-	IN project_name VARCHAR(255),
-	IN score  INT
-) 
-
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-	DECLARE project_id INT DEFAULT NULL;
+	DECLARE project_id INT DEFAULT -1;
+	
 	SELECT id INTO project_id FROM projects WHERE name = project_name;
-
-	if project_id = NULL THEN
-		INSERT INTO projects(name) VALUES(project_name);
+	
+	IF project_id = -1 THEN
+		INSERT INTO projects (name) VALUES (project_name);
 		SET project_id = LAST_INSERT_ID();
 	END IF;
-
-	INSERT INTO corrections(user_id, project_id, score) VALUES(user_id, project_id, score);
 	
+	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END;
-&&
+|
+
+DELIMITER ;-- SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
+
+DROP PROCEDURE IF EXISTS `AddBonus`;
+
+DELIMITER |
+
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+	DECLARE project_id INT DEFAULT -1;
+	
+	SELECT id INTO project_id FROM projects WHERE name = project_name;
+	
+	IF project_id = -1 THEN
+		INSERT INTO projects (name) VALUES (project_name);
+		SET project_id = LAST_INSERT_ID();
+	END IF;
+	
+	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END;
+|
 
 DELIMITER ;
