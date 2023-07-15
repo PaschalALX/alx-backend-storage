2001120e206958df25a44b4a0e3f92b906f0fbf0
PaschalALX <pasmac2018@gmail.com>
2023-07-15 23:47:58 +0100

    Added task 11

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,5 @@
+DROP VIEW IF EXISTS need_meeting; 
+
+CREATE VIEW need_meeting as SELECT name from students 
+WHERE score < 80 
+AND (last_meeting IS NULL OR DATE(last_meeting) > DATE_ADD(DATE(NOW()), INTERVAL -1 MONTH));
