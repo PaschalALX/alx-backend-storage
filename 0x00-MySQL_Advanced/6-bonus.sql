4a97c5d6dd20d12cf61cd9abb490598c6e61f0ca
PaschalALX <pasmac2018@gmail.com>
2023-07-15 17:57:11 +0100

    Ref 2

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -11,12 +11,12 @@ CREATE PROCEDURE AddBonus(
 ) 
 
 BEGIN
-	DECLARE project_id INT DEFAULT -1;
+	DECLARE project_id INT DEFAULT NULL;
 	SELECT id INTO project_id FROM projects WHERE name = project_name;
 
-	if project_id = -1 THEN
+	if project_id = NULL THEN
 		INSERT INTO projects(name) VALUES(project_name);
-		SET project_id = LAST_INSERTED_ID();
+		SET project_id = LAST_INSERT_ID();
 	END IF;
 
 	INSERT INTO corrections(user_id, project_id, score) VALUES(user_id, project_id, score);
