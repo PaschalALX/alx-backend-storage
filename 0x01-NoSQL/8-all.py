a1972ab2abc8d1d3b148dce5ae1d76af2b26be28
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-07-18 20:01:27 +0000

    Task 8 -1

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-
 """ Task 8 - List all documents in Python """
 
 def list_all(mongo_collection):
-""" lists all documents in a collection """
+    """ lists all documents in a collection """
 
-return [doc for doc in mongo_collection.find()]
+    return [doc for doc in mongo_collection.find()]
 
