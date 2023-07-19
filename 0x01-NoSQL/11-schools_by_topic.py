50d754f0c4925cd03a71ccb8d0e626373a05f397
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-07-19 04:11:44 +0000

    Added Task 12

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,4 +5,5 @@
 def schools_by_topic(mongo_collection, topic):
     """ returns the list of school having a specific topic  """
 
-    return mongo_collection.find({"topics": topic})
+    docs = mongo_collection.find({"topics": topic})
+		return list(docs)
--- 0x01-NoSQL/test.py
+++ 0x01-NoSQL/test.py
@@ -1,17 +1,17 @@
 import pymongo
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["my_db"]
-school_collection = db["school"]
-# school_collection.update_one()
-[print(doc) for doc in school_collection.find({"topics": "Python is great"})]
-# school_collection.update_many({
-#     "name": "Paschal"
-# }, 
-# {
-#     "$set":{
-#         "topics": [
-#         "Is PHP dead?", "Python is great"
-#     ]}
-# }
-# )
+client = pymongo.MongoClient("mongodb://localhost:27017")
+db = client["logs"]
+collection = db["nginx"]
+
+x_logs = collection.count_documents({})
+print("{} logs".format(x_logs))
+
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+print("Methods:")
+for method in methods:
+    method_log = collection.count_documents({"method": method})
+    print("\t method {}: {}".format(method, method_log))
+
+status_check = collection.count_documents({"method": "GET", "path": "/status"})
+print("{} status check".format(status_check))
