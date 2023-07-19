7ff3aac4833ed79d2aca975d3087aeede83bd8b6
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-07-19 03:36:08 +0000

    Added task 11

--- 0x01-NoSQL/test.py
+++ 0x01-NoSQL/test.py
@@ -3,8 +3,8 @@ import pymongo
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["my_db"]
 school_collection = db["school"]
-
-[print(doc) for doc in school_collection.find()]
+# school_collection.update_one()
+[print(doc) for doc in school_collection.find({"topics": "Python is great"})]
 # school_collection.update_many({
 #     "name": "Paschal"
 # }, 
