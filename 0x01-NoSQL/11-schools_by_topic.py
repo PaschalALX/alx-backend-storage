e342c038fb62437ad07c095b755c7805a7d00a51
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-07-19 04:46:59 +0000

    Added task 102

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,4 +6,4 @@ def schools_by_topic(mongo_collection, topic):
     """ returns the list of school having a specific topic  """
 
     docs = mongo_collection.find({"topics": topic})
-		return list(docs)
+    return list(docs)
