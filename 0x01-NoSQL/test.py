4c49a647c73e9ed97ac125e5ddf87ed762ef20f9
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-07-19 02:50:43 +0000

    Task 9

--- 0x01-NoSQL/test.py
+++ 0x01-NoSQL/test.py
@@ -4,6 +4,12 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["my_db"]
 school_collection = db["school"]
 
-schools = [doc for doc in school_collection.find()]
+me = school_collection.insert_one({"name":"Paschal", "age": 30})
+for x in me:
+    print(x)
 
-print(schools)
+# def insert_school(mongo_collection, **kwargs):
+#     mongo_collection.insert_one(kwargs)
+
+
+# insert_school(school_collection, name="Paschal", age=30)
